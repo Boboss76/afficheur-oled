1aa7f3ef2f91a70ec0375facfb363f4791f18323
lateteetlesmains <127963157+lateteetlesmains@users.noreply.github.com>
2023-03-15 19:25:12 +0100

    Mise à jour pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 OLED.init(128, 64)
-for index in range(3):
-    OLED.write_num_new_line(index)
-
-def on_forever():
-    pass
-basic.forever(on_forever)
+OLED.write_string_new_line("*********************")
+OLED.write_string_new_line("******    ****   ****")
+OLED.write_string_new_line("*********************")
+OLED.write_string_new_line("*******        ******")
+OLED.write_string_new_line("***********  ********")
+OLED.write_string_new_line("*********************")
--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 OLED.init(128, 64)
-basic.forever(function () {
-    for (let index = 0; index <= 5; index++) {
-        OLED.writeStringNewLine("*")
-    }
-})
+OLED.writeStringNewLine("*********************")
+OLED.writeStringNewLine("******    ****   ****")
+OLED.writeStringNewLine("*********************")
+OLED.writeStringNewLine("*******        ******")
+OLED.writeStringNewLine("***********  ********")
+OLED.writeStringNewLine("*********************")
