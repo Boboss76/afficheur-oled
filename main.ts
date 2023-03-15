63ccfc33cf96b8ab673d977010e259d19af76aad
lateteetlesmains <127963157+lateteetlesmains@users.noreply.github.com>
2023-03-15 21:59:25 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,39 @@
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
 OLED.init(128, 64)
+for (let index = 0; index <= 100; index++) {
+    OLED.drawLoading(index)
+}
+OLED.clear()
+OLED.writeStringNewLine("Bienvenue")
+basic.pause(500)
+OLED.clear()
 OLED.writeStringNewLine("*********************")
 OLED.writeStringNewLine("******    ****   ****")
 OLED.writeStringNewLine("*********************")
 OLED.writeStringNewLine("*******        ******")
-OLED.writeStringNewLine("***********  ********")
+OLED.writeStringNewLine("********     ********")
 OLED.writeStringNewLine("*********************")
+basic.forever(function () {
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . # . # .
+        . # . # .
+        `)
+    basic.pause(100)
+})
